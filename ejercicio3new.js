bcad4d532ac4f25957805d5fd517abd012cceaac
Kopos <carlosh.viveros12@gmail.com>
2023-06-13 13:47:11 +0200

    ejercicios curso javascript

--- ejercicio3new.js
+++ ejercicio3new.js
@@ -5,21 +5,22 @@ const movies = [
     { name: "The Lord of the Rings", durationInMinutes: 967 },
     { name: "Star Wars: A New Hope", durationInMinutes: 214 },
     { name: "Terminator", durationInMinutes: 140 },
-]   
+] 
+const finalsmallmovie = []  
 for (let i = 0; i < movies.length; i++) {
     const list = movies[i];
     console.log(list);
     if (movies.durationInMinutes < 100) {
-        smallmovies };
+        finalsmallmovie.push(movies) };
         console.log(smallmovies);
     
     if (movies.durationInMinutes > 100 && movie.durationInMinutes < 200) {
-        mediummovies};
+        finalmediummovie.push(movies) };
         console.log(mediummovies);
 
     if (movies.durationInMinutes > 200) {
-        bigmovies};
-        console.log(bigmovies);    
+        finallargemovies.push(movies)};
+        console.log(largemovies);    
 
 
 
