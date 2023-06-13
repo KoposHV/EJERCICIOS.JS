d172c3fc6e4bbe0d3ea4cfaa0714b37cdeb89849
Kopos <carlosh.viveros12@gmail.com>
2023-06-13 18:38:55 +0200

    ejercicios curso javascript

---
+++ ejercicio5new.js
@@ -0,0 +1,22 @@
+const products = [
+    { name: "Funko Dr. Strange", sellCount: 10 },
+    { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+    { name: "Sable laser FX", sellCount: 23 },
+    { name: "Varita de Voldemort", sellCount: 6 },
+    ];
+    let totalSales = 0;
+
+for (let i = 0; i < products.length; i++) {
+  totalSellCount += products[i].
+  totalSellCount += products[i].
+
+  totalSellCount += products
+
+  totalSales
+sellCount;
+}
+
+const averageSales = totalSellCount / products.length;
+
+console.log("Total de ventas: " + totalSellCount);
+console.log("Media de ventas: " + averageSales);
