ce6fd71570935a2e202a11a7445dbf83ef63fc60
Kopos <carlosh.viveros12@gmail.com>
2023-06-13 12:48:39 +0200

    ejercicios curso javascript

--- ejercicio2new.js
+++ ejercicio2new.js
@@ -7,9 +7,16 @@ const foodSchedule = [
 { name: "Rice", isVegan: true },
 { name: "Pasta", isVegan: true },
 ];
+const usedFruits = [];
 for (const i = 0; i < foodSchedule.length; i++) {
     if (!foodSchedule[i].isVegan) {
-        
-    }
-   
-}
+        letfruitIndex = Math.floor(Math.random()*fruits.length);
+        while
+        (usedFruits.includes(fruits[fruitIndex])){
+            fruitIndex = Math.floor(Math.random() * fruits.length);
+        }
+        foodSchedule[i].name = fruits[fruitIndex];
+        usedFruits.push(fruits[fruitIndex]);
+        console.log(foodSchedule);
+        }
+   }
