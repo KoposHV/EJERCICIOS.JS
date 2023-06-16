3710ee3a871ba7c1576ce93180506e28deb78d2e
Kopos <carlosh.viveros12@gmail.com>
2023-06-16 14:27:00 +0200

    ejercicios curso javascript

---
+++ ejercicio10.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+function sumAll(param) {
+    let = 0;
+    for (let i = 0; i<param.length; i++) {
+        sum += param[i];
+    }
+    return sum;
+
+    console.log(sumAll)
+}
+
+
