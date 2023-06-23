41f40d2347151789d37e8029fc8e3212e487d57b
Kopos <carlosh.viveros12@gmail.com>
2023-06-23 11:01:52 +0200

    ejercicios curso javascript

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,12 +1,14 @@
 const numbers = [12, 21, 38, 5, 45, 37, 6];
 function average(param) {
-    let average = numbers / numbers.length;
-    for (let i = 0; i < param.length; i++) {
-        average = param[i];
-    }   
-    console,log(average); 
-
+    let total = 0;
+    for (const number of param) {
+        total += number;
     }
+    
+       const quantity = param.length;
+       console.log(total / quantity);      
+       }
+       average(numbers);
     
 
 
